f5f86c458084221db06d221c102a5b56ec4dc1b4
Indrajaiswal <indra.191513@ncit.edu.np>
2023-07-08 22:20:23 +0545

    backend update

--- app1/views.py
+++ app1/views.py
@@ -46,3 +46,15 @@ def LogoutPage(request):
     
 def Dashboard(request):
     return render (request,'dashboard.html')  
+
+def about(request):
+    return render (request,'about.html')
+
+def services(request):
+    return render (request,'services.html')
+
+def insurance(request):
+    return render (request,'insurance.html')
+
+
+
